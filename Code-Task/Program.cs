cb24fcbb9229a9915e6238d8037bee05cd92516d
MammadovShahin <shahinkm@code.edu.az>
2021-08-17 09:51:04 +0400

    Task2

---
+++ Code-Task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Code_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 10, 20, 62, 4 };
+            Console.WriteLine(Sum(arr));
+        }
+        static int Sum (int[] arr)
+        {
+           int result = 0;
+           int sum=0;
+            int j=0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                j++;
+            }
+            result = sum / j;
+
+            return result;
+
+        } 
+    }
+}
